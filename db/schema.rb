8bbd6909e21f6697fcdcd4a3dbaea92d580a48a1
Antonino <antoninoscaffidi@gmail.com>
2018-01-30 20:20:31 +0100

    Added last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130174608) do
+ActiveRecord::Schema.define(version: 20180130191446) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180130174608) do
     t.string "uid"
     t.string "image"
     t.string "name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
