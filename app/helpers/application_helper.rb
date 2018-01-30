6dadefedf0c8505c408717c7057d97f429e76cb1
Antonino <antoninoscaffidi@gmail.com>
2018-01-30 20:00:58 +0100

    Added name to users table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,24 @@
 module ApplicationHelper
 
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.email)
+    if user.image
+      image_tag("https://graph.facebook.com/#{user.uid}/picture?type=large")
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, alt: user.email)
+    end
   end
 
 
   def gravatar_navbar(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.email, width: "30", height: "30")
+     if user.image
+      image_tag("https://graph.facebook.com/#{user.uid}/picture?type=small", width: "30", height: "30")
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, alt: user.email, width: "30", height: "30")
+    end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124165848) do
+ActiveRecord::Schema.define(version: 20180130174608) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180124165848) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
