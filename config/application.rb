8530a34fe277842a5897ab02e985f84a483b4d39
Antonino <antoninoscaffidi@gmail.com>
2018-07-03 13:24:13 +0200

    Updated to rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,16 @@ Bundler.require(*Rails.groups)
 module Giosando
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     
     #config.time_zone = "Rome"
     #I18n.available_locales = [:it, :en]
     #config.i18n.default_locale = :it
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     console do 
       ActiveRecord::Base.connection
     end
